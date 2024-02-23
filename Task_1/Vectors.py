65ee067b69696f32cb09699b9e7bf433c45d92eb
Ivan Pavlenko <ivan_pavlenko@knu.ua>
2024-02-23 20:30:18 +0200

    added copy support

--- Task_1/Vectors.py
+++ Task_1/Vectors.py
@@ -1,6 +1,19 @@
 class Vector:
-    def __init__(self, *coordinates):
-        self.coordinates = coordinates
+    def __init__(self, first_arg, *arguments):
+        if isinstance(first_arg, Vector):
+            assert len(arguments) == 0
+
+            self.coordinates = first_arg.coordinates
+
+        else:
+            assert isinstance(first_arg, int) or isinstance(first_arg, float)
+
+            for arg in arguments:
+                assert isinstance(arg, int) or isinstance(arg, float)
+
+            coordinates = (None, first_arg) + arguments
+
+            self.coordinates = coordinates[1:]
 
     def __str__(self):
         return f'{self.coordinates}'
@@ -11,8 +24,8 @@ class Vector:
     def length(self):
         res = 0
 
-        for c in self.coordinates:
-            res += c ** 2
+        for crd in self.coordinates:
+            res += crd ** 2
 
         return res ** 0.5
 
@@ -22,9 +35,9 @@ class Vector:
     def max(self):
         maximum = self.coordinates[0]
 
-        for c in self.coordinates[1:]:
-            if maximum < c:
-                maximum = c
+        for crd in self.coordinates[1:]:
+            if maximum < crd:
+                maximum = crd
 
         return maximum
 
@@ -34,8 +47,8 @@ class Vector:
 
         minimum = self.coordinates[0]
 
-        for c in self.coordinates[1:]:
-            if minimum > c:
-                minimum = c
+        for crd in self.coordinates[1:]:
+            if minimum > crd:
+                minimum = crd
 
         return minimum
