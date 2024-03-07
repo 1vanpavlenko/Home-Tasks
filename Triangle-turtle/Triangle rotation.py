479bbeb241e4d98adce2d1712b06aaac2f26e656
Ivan Pavlenko <ivan_pavlenko@knu.ua>
2024-03-07 21:49:55 +0200

    complited Triangle rotation

--- Triangle-turtle/Triangle rotation.py
+++ Triangle-turtle/Triangle rotation.py
@@ -3,22 +3,28 @@
 import turtle
 from Triangle import Triangle
 from time import sleep
+from functions import calc_length
 
 vertex_1 = [float(_) for _ in input("Enter first vertex (any two numbers): ").split()]
 assert len(vertex_1) == 2
 vertex_2 = [float(_) for _ in input("Enter second vertex (any two numbers): ").split()]
 assert len(vertex_2) == 2
-position = [float(_) for _ in input("Enter third vertex (position): ").split()]
+position = [float(_) for _ in input("Enter position: ").split()]
 assert len(position) == 2
 
-a = ((vertex_1[0] - vertex_2[0]) ** 2 + (vertex_1[1] - vertex_2[1]) ** 2) ** 0.5       # position
-b = ((position[0] - vertex_2[0]) ** 2 + (position[1] - vertex_2[1]) ** 2) ** 0.5       # vertex_1
-c = ((position[0] - vertex_1[0]) ** 2 + (position[1] - vertex_1[1]) ** 2) ** 0.5       # vertex_2
-
-rotation_point = ((a * position[0] + b * vertex_1[0] + c * vertex_2[0]) / (a + b + c),
-                  (a * position[1] + b * vertex_1[1] + c * vertex_2[1]) / (a + c + b))
-
 triangle = Triangle(*vertex_1, *vertex_2)
+
+current_vertex_1 = vertex_1[0] + position[0], vertex_1[1] + position[1]
+current_vertex_2 = vertex_2[0] + position[0], vertex_2[1] + position[1]
+
+a = calc_length(current_vertex_1, current_vertex_2)
+b = calc_length(position, current_vertex_2)
+c = calc_length(position, current_vertex_1)
+# a - position, b - vertex_1, c - vertex_2
+# rotation point - точка перетину бісектрис
+rotation_point = ((a * position[0] + b * current_vertex_1[0] + c * current_vertex_2[0]) / (a + b + c),
+                  (a * position[1] + b * current_vertex_1[1] + c * current_vertex_2[1]) / (a + b + c))
+
 triangle.set_position(position)
 triangle.set_rotation_point(rotation_point)
 sleep(3)
